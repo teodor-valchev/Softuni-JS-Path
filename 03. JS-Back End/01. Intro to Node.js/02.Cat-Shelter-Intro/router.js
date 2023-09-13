a45ddbb53123035a090e7f16f3c74feb9f1c56eb
teodor.valchev <teodor.valchev@merklecxm.com>
2023-09-13 17:36:43 +0300

    Initialize project

---
+++ 03. JS-Back End/01. Intro to Node.js/02.Cat-Shelter-Intro/router.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const homeTemplate = fs.readFileSync('./views/home/index.html',{ encoding: 'utf-8' });
+const cssFile = fs.readFileSync('./content/styles/site.css',{ encoding: 'utf-8' });
+const catsBreedTemplate = fs.readFileSync('./views/addBreed.html',{ encoding: 'utf-8' });
+const addCatTemplate = fs.readFileSync('./views/addCat.html',{ encoding: 'utf-8' });
+
+const catsDb = fs.readFileSync('./data/catsDB.json', { encoding: 'utf-8' })
+
+
+function router (url, res) {
+    if (url === '/') {
+        const parsedCats = JSON.parse(catsDb);
+        parsedCats.cats.forEach(cat => {
+            res.write(homeTemplate.replaceA('name',cat.name));
+            
+        });
+        console.log(homeTemplate);
+        //res.write(homeTemplate);
+    } else if (url === '/content/styles/site.css') {
+        res.write(cssFile);
+    } else if (url === '/cats/add-breed') {
+        res.write(catsBreedTemplate);
+    } else if (url === '/cats/add-cat') {
+        res.write(addCatTemplate);
+    }
+    res.end();
+}
+
+module.exports = router;
