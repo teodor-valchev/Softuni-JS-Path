a841c5c91dbb54df7a42010b63d8a1b407764d77
teodor.valchev <teodor.valchev@merklecxm.com>
2023-09-13 16:29:23 +0300

    Refactoring catshelter

--- 03. JS-Back End/01. Intro to Node.js/01. Cat-Shelter/src/config/viewEngine.js
+++
@@ -1,9 +0,0 @@
-const handlebars = require("express-handlebars");
-
-function viewEngine (app) {
-    app.engine("hbs", handlebars.engine({ extname: "hbs"}));
-    app.set("view engine", "hbs");
-    app.set("views", "./src/views/");
-}
-
-module.exports = viewEngine
