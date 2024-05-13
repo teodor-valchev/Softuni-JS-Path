838659d3b9f6486f0c8580d110b858deb7bbc6aa
teo <teo.thenx@gmail.com>
2024-05-13 18:05:01 +0300

    removed old implementation of cat shelter

--- 03. JS-Back End/01. Intro to Node.js/01. Cat-Shelter-Express/src/config/expressConfig.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const path = require("path");
-
-module.exports = (app) => {
-    app.use(express.static(path.resolve(__dirname, "../public")));
-    app.use(express.urlencoded({ extended: true }));
-};
