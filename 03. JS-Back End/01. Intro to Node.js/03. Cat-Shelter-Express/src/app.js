667ce8e64b0f74668ef32e0f2225fd3d14528c6c
teo <teo.thenx@gmail.com>
2024-05-13 13:20:58 +0300

    Setup handlebars Added views Setup static files

--- 03. JS-Back End/01. Intro to Node.js/03. Cat-Shelter-Express/src/app.js
+++ 03. JS-Back End/01. Intro to Node.js/03. Cat-Shelter-Express/src/app.js
@@ -1,10 +1,31 @@
 const express = require("express");
+const handlebars = require('express-handlebars')
+const path = require('path')
 
 const app = express();
 const PORT = 3000;
 
+//handlebars setup
+app.engine('hbs', handlebars.engine({
+    extname:'hbs'
+}))
+app.set('view engine', 'hbs')
+//views set
+app.set('views', 'src/views');
+
+// static files
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.get("/", (req, res) => {
-    res.send("Hello");
+    res.render("home");
+});
+
+app.get("/cats/add-cat", (req, res) => {
+    res.render("addCat");
+});
+
+app.get("/cats/add-breed", (req, res) => {
+    res.render("addBreed");
 });
 
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}...`));
