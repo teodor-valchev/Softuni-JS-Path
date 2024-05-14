26d52d3b64c3270d47e6c9ddd419bcb5df07cdce
teo <teo.thenx@gmail.com>
2024-05-14 16:16:12 +0300

    Reinstalled packages, to update them setup the home page

--- 03. JS-Back End/02. ExpressJS and Templating/03. Cubicle/src/app.js
+++ 03. JS-Back End/02. ExpressJS and Templating/03. Cubicle/src/app.js
@@ -1,10 +1,18 @@
 const express = require('express')
+const hbsConfig = require('./config/hbsConfiguration');
+const expressConfig = require('./config/expressConfig');
 
 const app = express()
 const PORT = 3000;
 
+//hbs setup
+hbsConfig(app)
+
+//expressConfig
+expressConfig(app)
+
 app.get('/', (req, res) => {
-    res.send('Helo')
+    res.render('home', {layout: false})
 })
 
 app.listen(PORT, () => console.log(`Server is running on poort: ${PORT}...`))
