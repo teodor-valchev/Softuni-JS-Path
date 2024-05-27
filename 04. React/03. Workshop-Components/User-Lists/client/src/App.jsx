d407262f639899b95e80d2fc0cd4817f4f1bf44f
teodor-valchev <teo.thenx@gmail.com>
2024-05-27 13:15:58 +0300

    Initial commit for creating UserList app structure

---
+++ 04. React/03. Workshop-Components/User-Lists/client/src/App.jsx
@@ -0,0 +1,17 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import UserList from "./components/UserList";
+
+function App() {
+    return (
+        <>
+            <Header />
+            <main className="main">
+                <UserList />
+            </main>
+            <Footer />
+        </>
+    );
+}
+
+export default App;
