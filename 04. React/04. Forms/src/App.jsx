8d256e64343a23b9ee11a5b5399594db555c266b
teodor-valchev <teo.thenx@gmail.com>
2024-05-29 17:15:43 +0300

    Initial  commit for exercising controlled forms

---
+++ 04. React/04. Forms/src/App.jsx
@@ -0,0 +1,12 @@
+import MyControlledForm from "./components/MyCountrolledForm";
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <MyControlledForm />
+        </>
+    );
+}
+
+export default App;
