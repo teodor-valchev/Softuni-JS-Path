0f37c76cbbc5b4acb0ad53137d90daa7c04319f8
teodor-valchev <teo.thenx@gmail.com>
2024-06-03 19:32:42 +0300

    implemented create and catalog views

--- 04. React/06. Workoshop-Routing/Games-Play/client/src/App.jsx
+++ 04. React/06. Workoshop-Routing/Games-Play/client/src/App.jsx
@@ -2,8 +2,10 @@ import { Route, Routes } from "react-router-dom";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import Login from "./components/login/Login";
-import "./App.css";
 import Register from "./components/register/Register";
+import Catalog from "./components/all-games/Catalog";
+import Create from "./components/create-game/Create";
+import "./App.css";
 
 function App() {
     return (
@@ -11,6 +13,8 @@ function App() {
             <Header />
             <Routes>
                 <Route path="/" element={<Home />}></Route>
+                <Route path="/all-games" element={<Catalog />}></Route>
+                <Route path="/create" element={<Create />}></Route>
                 <Route path="/login" element={<Login />}></Route>
                 <Route path="/register" element={<Register />}></Route>
             </Routes>
