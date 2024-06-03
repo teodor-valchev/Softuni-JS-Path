05b5d9d8714dc5abed31e8c4f9d7b808f994c89b
teodor-valchev <teo.thenx@gmail.com>
2024-06-03 19:21:11 +0300

    Installed Router library and added login and register page

--- 04. React/06. Workoshop-Routing/Games-Play/client/src/App.jsx
+++ 04. React/06. Workoshop-Routing/Games-Play/client/src/App.jsx
@@ -1,16 +1,21 @@
-import Header from "./components/header/Header"
-import Home from "./components/home/Home"
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/header/Header";
+import Home from "./components/home/Home";
+import Login from "./components/login/Login";
+import "./App.css";
+import Register from "./components/register/Register";
 
 function App() {
-
-  return (
-    <div id="box">
-      <Header />
-      <Home />
-      
-    </div>
-  )
+    return (
+        <div id="box">
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="/login" element={<Login />}></Route>
+                <Route path="/register" element={<Register />}></Route>
+            </Routes>
+        </div>
+    );
 }
 
-export default App
+export default App;
